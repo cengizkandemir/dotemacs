56e556fe841b969a9bc955d2ff484b7d2cb65665
Cengiz Kandemir <cengizkandemir@live.com>
2020-02-08 20:35:50 +0100

    Use a separate file for customization

--- init.el
+++ init.el
@@ -10,21 +10,6 @@
 
 (package-initialize)
 
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (pdf-tools auctex cmake-mode magit zenburn-theme use-package restart-emacs nyan-mode lsp-ui flycheck fill-column-indicator company-lsp ccls bug-hunter))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
-
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;; cosmetics ;;;;;;;;;;;;;;;;;
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -69,6 +54,9 @@
 
 (desktop-save-mode 1)
 (save-place-mode t)
+
+(setq custom-file "~/.emacs.d/custom.el")
+(load custom-file :noerror)
 
 (global-set-key (kbd "H-p") 'windmove-up)
 (global-set-key (kbd "H-n") 'windmove-down)
